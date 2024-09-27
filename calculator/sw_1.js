a4bbef36946ec9a7cb8324773d4d4a3cc77e991e
Ben Groeneveld (0912716) <0912716@hr.nl>
2024-09-27 14:51:54 +0200

    changed logo refs in sw

--- calculator/sw_1.js
+++ calculator/sw_1.js
@@ -1,12 +1,12 @@
 self.addEventListener("install", e => {
     console.log("Installing sw");
-	console.log("Caching resources..");
+    console.log("Caching resources..");
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
                 "./",
                 "./src/manifest.json",
-                "./src/images/logo192.png"
+                "./src/images/vrl192.png"
             ]);
         })
     );
